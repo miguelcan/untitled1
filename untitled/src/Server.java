f67dabfd783f1cd874241a9e4497f09d350a6994
miguelcan <vandit666@hotmail.com>
2013-11-05 11:57:30 -0600

    First

---
+++ untitled/src/Server.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Miguel
+ * Date: 30/10/13
+ * Time: 03:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Server {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JSpinner spinner1;
+    private JTextField textField1;
+    private JButton detenerButton;
+    private JCheckBox timeoutCheckBox;
+}
